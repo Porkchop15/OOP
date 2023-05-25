2651afc44d1007a338af548c484f78a3436562c3
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2023-05-25 13:23:13 +0800

    Create FandangoYacht.java

---
+++ Attendance/May25/FandangoYacht.java
@@ -0,0 +1,14 @@
+package may25;
+
+public class FandangoYacht extends Vehicle {
+	String MainSailColor = "";
+	
+	void Float() {
+		System.out.println("The boat is sailing");
+	}
+	
+	@Override
+	void Stop() {
+		super.Stop();
+	}
+}
