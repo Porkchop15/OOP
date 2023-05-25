821e606e729d9e0845e0cb3b65408da1e6eb7f7b
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2023-05-25 13:23:49 +0800

    Create Toyota_VIOS.java

---
+++ Attendance/May25/Toyota_VIOS.java
@@ -0,0 +1,15 @@
+package may25;
+
+public class Toyota_VIOS extends Vehicle{
+	 String TireType = "Performance Tire";
+		
+		void Drive() {
+			System.out.println("The Car is accelerating");
+		}
+		
+
+		@Override
+		void Stop() {
+			super.Stop();
+		}
+}
