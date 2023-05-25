a9fa7d6304bfa43e78b82b7ea58e4a5e48578251
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2023-05-25 13:24:07 +0800

    Create U2_SpyPlane.java

---
+++ Attendance/May25/U2_SpyPlane.java
@@ -0,0 +1,16 @@
+package may25;
+
+public class U2_SpyPlane extends Vehicle {
+		
+		 int WingSpan = 103;
+			
+			void Fly() {
+				System.out.println("The Plane is flying");
+			}
+			
+			
+			@Override
+			void Stop() {
+				super.Stop();
+			}
+}
