193c7fcfe5e77bd8248cdee92f2d46dd5fd49c65
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2023-06-05 08:54:19 +0800

    Create portfolio.java

---
+++ Portfolio/portfolio.java
@@ -0,0 +1,69 @@
+package personalportfolio;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class portfolio extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					portfolio frame = new portfolio();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	
+	/**
+	 * Create the frame.
+	 */
+	public portfolio() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 475);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				table table = new table();
+				table.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(345, 312, 89, 47);
+		btnNewButton.setOpaque(false);
+		btnNewButton.setContentAreaFilled(false);
+		btnNewButton.setBorderPainted(false);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("D:\\New folder\\hugo\\College\\PROGRAMMING 3\\1.jpg"));
+		lblNewLabel.setBounds(0, 0, 800, 450);
+		contentPane.add(lblNewLabel);
+	}
+	
+
+}
